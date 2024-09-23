144c48a0e23f3f824afcc8ffafaa394d67cf57a9
koneahmedsalim <koneahmedsalim@gmail.com>
2024-09-23 00:36:08 +0000

    maintenant on peut pas reserer plus de 12 places par secretaire avant on pouvais pas reserver plus de 12 places par competition mais tant qu\il y avait des places et des points on pouvait reserver 12 places puis revenir reserver des places en plus elevant le total  a plus de 12 CORRIGER

--- server.py
+++ server.py
@@ -20,6 +20,8 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+reservations_by_club = {}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -57,26 +59,40 @@ def purchasePlaces():
     placesRequired = int(request.form['places'])
     total_points_needed = placesRequired * 1
     
-    if placesRequired > 12:
-        flash("Vous ne pouvez pas réserver plus de 12 places pour une compétition.")
+    # Initialiser le dictionnaire si nécessaire
+    if club['name'] not in reservations_by_club:
+        reservations_by_club[club['name']] = {}
+
+    if competition['name'] not in reservations_by_club[club['name']]:
+        reservations_by_club[club['name']][competition['name']] = 0
+
+    # Calculer le total des réservations existantes pour ce club dans cette compétition
+    total_reserved_by_club = reservations_by_club[club['name']][competition['name']]
+
+    # Vérification : le club ne peut pas réserver plus de 12 places au total
+    if total_reserved_by_club + placesRequired > 12:
+        flash(f"Vous ne pouvez pas réserver plus de 12 places au total pour cette compétition.")
         return render_template('welcome.html', club=club, competitions=competitions)
-    
+
+    # Vérification du nombre de places disponibles
     if placesRequired > int(competition['numberOfPlaces']):
         flash(f"Il ne reste que {competition['numberOfPlaces']} places disponibles.")
         return render_template('welcome.html', club=club, competitions=competitions)
-    if int(club['points']) >= total_points_needed:  
-        # Vérifier s'il y a assez de places disponibles dans la compétition
-        if int(competition['numberOfPlaces']) >= placesRequired:
-            # Deduct points and update places
-            club['points'] = int(club['points']) - total_points_needed  
-        else:
-            flash('Il n\'y a pas assez de places disponibles pour cette compétition.')
+
+    # Vérification que le club a assez de points pour réserver
+    total_points_needed = placesRequired
+    if int(club['points']) >= total_points_needed:
+        # Mettre à jour les points et les places disponibles
+        club['points'] = int(club['points']) - total_points_needed
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+
+        # Mettre à jour le nombre total de places réservées par le club
+        reservations_by_club[club['name']][competition['name']] += placesRequired
+
+        flash(f"Réservation réussie ! Vous avez réservé {placesRequired} places.")
     else:
-        flash('Vous n\'avez pas assez de points pour réserver ces places.')
-    
-    
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
+        flash("Vous n'avez pas assez de points pour réserver ces places.")
+
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
